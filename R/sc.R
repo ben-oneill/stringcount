affa0bf5dfc40cee3f90f385aa4b6f21e4b8821f
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2021-01-28 13:20:00 +1100

    Update and rename s.R to sc.R

--- R/sc.R
+++ R/sc.R
@@ -1,24 +1,23 @@
 #' Split/concatenate between a single character string and a vector of its individual symbols
 #'
-#' \code{s} can be used to split a single character string into a vector of its individual symbols, or concatenate a character
-#' vector with multiple elements into a single character string.  The user may set the logical value ```split``` to specify if they wish to
-#' split or concatenate the ```string``` input.  If this value is unspecified then the function will determine whether to split or concatenate
-#' by looking at the length of the ```string``` vector; if this vector has a single element it will be split; if it has multiple elements it
+#' \code{sc} can be used to split a single character string into a vector of its individual symbols, or concatenate a character
+#' vector with multiple elements into a single character string.  The user may set the logical value \code{split} to specify if they wish to
+#' split or concatenate the \code{string} input.  If this value is unspecified then the function will determine whether to split or concatenate
+#' by looking at the length of the \code{string} vector; if this vector has a single element it will be split; if it has multiple elements it
 #' will be concatenated.  (Note: if the user concatenates a string vector with one or more elements that have multiple characters, they will
 #' get a warning to alert the to the fact that concatenation loses the distinction between individual symbols and longer string of symbols.)
-#' If the user split the ```string``` then the function will return a character vector showing the individual symbols in the input string; if
-#' the user concatenated the ```string``` then the function will return a single character value with the concatenated string.
+#' If the user split the \code{string} then the function will return a character vector showing the individual symbols in the input string; if
+#' the user concatenated the \code{string} then the function will return a single character value with the concatenated string.
 #'
-#' @usage \code{s()}
+#' @usage \code{sc()}
 #' @param string A character value/vector
-#' @param split Logical; if ```TRUE``` the function will split the string; if ```FALSE``` the function will concatenate the string
-#' @param warning Logical; if ```TRUE``` the function will give a warning for concatenation of strings with elements that are not single characters
+#' @param split Logical; if \code{TRUE} the function will split the string; if \code{FALSE} the function will concatenate the string
+#' @param warning Logical; if \code{TRUE} the function will give a warning for concatenation of strings with elements that are not single characters
 #' @return A character vector giving the split/concatenated string.
 
-s <- function(string, split = NULL, warning = TRUE) {
+sc <- function(string, split = NULL, warning = TRUE) {
 
   #Check input
-  if (!is.vector(string))                                      { stop('Error: string must be a character vector') }
   if (!is.character(string))                                   { stop('Error: string must be a character vector') }
   if (length(string) == 0)                                     { stop('Error: string must have at least one element') }
   if (!missing(split)) {
