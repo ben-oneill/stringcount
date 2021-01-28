ab1088238e6d747a28826f759bfd0a273be2a2d6
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2021-01-28 13:22:07 +1100

    Add files via upload

--- R/stringcount.R
+++ R/stringcount.R
@@ -2,18 +2,18 @@
 #'
 #' \code{stringcount} returns the number of occurrances of a string in a text
 #'
-#' This function takes a ```text``` that is either a single character value or a vector of values, and a ```string``` that is either a single
+#' This function takes a \code{text} that is either a single character value or a vector of values, and a \code{string} that is either a single
 #' character value or a vector of values.  It counts the number of occurrances of the string in the text and returns this count.  By default
-#' the function counts overlapping strings as separate occurrances (i.e., they both add to the count), however the user can set ```allow.overlap```
-#' to ```FALSE``` to disallow occurrances of the string that overlap with previously counted occurrances (i.e., the latter occurrance does not
+#' the function counts overlapping strings as separate occurrances (i.e., they both add to the count), however the user can set \code{allow.overlap}
+#' to \code{FALSE} to disallow occurrances of the string that overlap with previously counted occurrances (i.e., the latter occurrance does not
 #' add to the count).  If the inputs are single character strings then the function checks for the occurrance of the string as a substring of the
 #' text.  If the inputs are longer vectors of numeric or character values then the elements of the vectors are treated as individual symbols and
 #' the vector gives the string of symbols that is checked in the count.
 #'
 #' @usage \code{stringcount()}
 #' @param text Either a single character string or a numeric/character vector
-#' @param string Either a single character string or a numeric/character vector (must have the same type as the ```text```)
-#' @param allow.overlap Logical; if ```TRUE``` then string occurrances are counted even if they overlap with previously counted occurrances
+#' @param string Either a single character string or a numeric/character vector (must have the same type as the \code{text})
+#' @param allow.overlap Logical; if \code{TRUE} then string occurrances are counted even if they overlap with previously counted occurrances
 #' @return The count value for the number of times the string occurs in the text
 
 stringcount <- function(text, string, allow.overlap = TRUE) {
